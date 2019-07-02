15ca4714cad080670ab15a65c7e56cca04f3a858
David <david.windler@windowslive.com>
2019-07-02 22:11:15 +0200

    mobile header styling

--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/events.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -94,6 +94,28 @@
 /***/ (function(module, exports) {
 
 eval("window.addEventListener(\"DOMContentLoaded\", () => {\n    const filter = {\n        hideLocations: {},\n        hideEvents: {}\n    };\n\n    addCheckboxListener(filter);\n    addToggleMoreInformationListener();\n});\n\nfunction addCheckboxListener(filter) {\n    const locationCheckboxes = document.getElementsByClassName(\n        \"filter__checkbox--location\"\n    );\n    const eventCheckboxes = document.getElementsByClassName(\n        \"filter__checkbox--event\"\n    );\n\n    Array.from(eventCheckboxes).forEach(checkbox =>\n        checkbox.addEventListener(\"click\", event => {\n            const eventName = event.target.value;\n            const hide = !event.target.checked;\n\n            filter.hideEvents[eventName] = hide;\n\n            toggleEvent(filter);\n        })\n    );\n\n    Array.from(locationCheckboxes).forEach(checkbox =>\n        checkbox.addEventListener(\"click\", event => {\n            const locationName = event.target.value;\n            const hide = !event.target.checked;\n\n            filter.hideLocations[locationName] = hide;\n\n            toggleEvent(filter);\n        })\n    );\n}\n\nfunction toggleEvent(filter) {\n    const eventElements = document.getElementsByClassName(\"event\");\n\n    Array.from(eventElements).forEach(event => {\n        eventName = event.querySelector(\".event__name\").textContent;\n        eventLocation = event.querySelector(\".event__location\").textContent;\n\n        const isHidden =\n            filter.hideEvents[eventName] || filter.hideLocations[eventLocation];\n\n        if (isHidden) {\n            event.style.display = \"none\";\n        } else {\n            event.style.display = \"block\";\n        }\n    });\n}\n\nfunction addToggleMoreInformationListener() {\n    const openMoreButtons = document.getElementsByClassName(\n        \"event__open-more-information\"\n    );\n    Array.from(openMoreButtons).forEach(button =>\n        button.addEventListener(\"click\", () => {\n            const event = button.closest(\".event\");\n            const moreInformation = event.querySelector(\n                \".event__more-information\"\n            );\n            moreInformation.classList.toggle(\"event__more-information--hidden\");\n        })\n    );\n}\n\n\n//# sourceURL=webpack:///./src/js/events.js?");
+
+/***/ }),
+
+/***/ "./src/js/navigation.js":
+/*!******************************!*\
+  !*** ./src/js/navigation.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("const hamburgerOpenClass = \"hamburger-button--open\";\nconst menuOpenClass = \"menu--open\";\n\nwindow.addEventListener(\"DOMContentLoaded\", () => {\n    addHamburgerButtonListener();\n});\n\nfunction addHamburgerButtonListener() {\n    const hamburgerButton = document.getElementsByClassName(\n        \"hamburger-button\"\n    )[0];\n\n    if (!hamburgerButton) {\n        console.warn(\"could not find menu button\");\n        return;\n    }\n\n    const menu = document.getElementsByClassName(\"menu\")[0];\n\n    if (!menu) {\n        console.warn(\"could not find menu\");\n        return;\n    }\n\n    hamburgerButton.addEventListener(\"click\", () => {\n        hamburgerButton.classList.toggle(hamburgerOpenClass);\n        const wasOpened = menu.classList.toggle(menuOpenClass);\n        document.body.style.overflow = wasOpened ? \"hidden\" : \"visible\"; // prevent scroll\n    });\n}\n\n\n//# sourceURL=webpack:///./src/js/navigation.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!*******************************************************!*\
+  !*** multi ./src/js/navigation.js ./src/js/events.js ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! ./src/js/navigation.js */\"./src/js/navigation.js\");\nmodule.exports = __webpack_require__(/*! ./src/js/events.js */\"./src/js/events.js\");\n\n\n//# sourceURL=webpack:///multi_./src/js/navigation.js_./src/js/events.js?");
 
 /***/ })
 
